49002365c0f3347ce7268a59ee7bbc235f16f7bd
Gabriel Medeiros <Gabriel Medeiros@DESKTOP-OICKAM7>
2019-09-06 12:13:50 -0300

    Post e Get

--- src/java/Controle/GeneroWS.java
+++ src/java/Controle/GeneroWS.java
@@ -5,13 +5,16 @@
  */
 package Controle;
 
+import dao.GeneroDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import modelo.Genero;
 
 /**
  *
@@ -24,24 +27,50 @@ public class GeneroWS extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String acao = request.getParameter("txtAcao");
-        
+        RequestDispatcher destino;
+        String pagina;
         switch(String.valueOf(acao)){
             case "add":
-                
+                pagina = "add.jsp";
                 break;
             case "edit":
-                
+                pagina = "edit.jsp";
+                break;
+            case "del":
+                pagina = "list.jsp";
                 break;
             default:
-                
+                String filtro = request.getParameter("filtro");
+                if(filtro==null){
+                    
+                }
+                else{
+                    
+                }
+                pagina = "list.jsp";
+                break;
         }
+        destino = request.getRequestDispatcher(pagina);
+        destino.forward(request, response);
     
     }
     
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        
+    Genero obj;
+    GeneroDAO dao = new GeneroDAO();
     
+    String id = request.getParameter("txtId");
+    
+    if(id!=null){
+        obj = dao.buscarPorChavePrimaria(Long.parseLong(id));
+    }
+    else{
+        obj = new Genero();
+    }
     }
     
 
